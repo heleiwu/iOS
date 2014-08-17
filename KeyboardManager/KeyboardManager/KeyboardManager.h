1675f5a05e5de8bc5acf1e45042e3807bad5ad94
Ryan <913495245@qq.com>
2014-08-17 21:32:25 +0800

    Init the KeyboardManager project.

---
+++ KeyboardManager/KeyboardManager/KeyboardManager.h
@@ -0,0 +1,13 @@
+//
+//  KeyboardManager.h
+//  KeyboardManager
+//
+//  Created by heleiwu on 8/17/14.
+//  Copyright (c) 2014 heleiwu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface KeyboardManager : NSObject
+
+@end
---
+++ KeyboardManager/KeyboardManager/KeyboardManager.m
@@ -0,0 +1,13 @@
+//
+//  KeyboardManager.m
+//  KeyboardManager
+//
+//  Created by heleiwu on 8/17/14.
+//  Copyright (c) 2014 heleiwu. All rights reserved.
+//
+
+#import "KeyboardManager.h"
+
+@implementation KeyboardManager
+
+@end
